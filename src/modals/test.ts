bf04f65d431f3e4bcfcd13d08129ca33bd56b0d8
MazenMarei <moazmazen9199@gmail.com>
2024-02-02 23:18:26 +0200

    remove

--- src/modals/test.ts
+++
@@ -1,8 +0,0 @@
-import { CommandInteraction } from "discord.js"; // Import CommandInteraction type
-
-export default {
-    id: "test",
-    function: async function ({ interaction, choices }: { interaction: CommandInteraction, choices: any }) {
-        // Your function code here
-    }
-};
--- src/selectMenus/test.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInteraction } from "discord.js"; // Import CommandInteraction type
-
-export default {
-    id: "test",
-    cooldown: 0,
-    function: async function ({ interaction, choices }: { interaction: CommandInteraction, choices: any }) { // Add type annotations
-        interaction.reply("test");
-    }
-};
